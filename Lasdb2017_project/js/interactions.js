537bb23575c0661d43f386c27cb6f5e09455bae2
Daniel Alejandro Garavito Aguilar <danielgaravito@gmail.com>
2016-08-17 16:10:56 -0500

    Avance2

--- Lasdb2017_project/js/interactions.js
+++ Lasdb2017_project/js/interactions.js
@@ -27,5 +27,23 @@ function loadText() {
 	};
 	xhttp.open("GET","textos/noticia_resumenes.txt", true);
 	xhttp.send();
+
+	var xhttp2 = new XMLHttpRequest();
+	xhttp2.onreadystatechange = function() {
+		if (xhttp2.readyState == 4 && xhttp2.status == 200) {
+			document.querySelector("#text2").innerHTML = xhttp2.responseText;		
+		}
+	};
+	xhttp2.open("GET","textos/noticia_inscripciones.txt", true);
+	xhttp2.send();
+
+		var xhttp3 = new XMLHttpRequest();
+	xhttp3.onreadystatechange = function() {
+		if (xhttp2.readyState == 4 && xhttp3.status == 200) {
+			document.querySelector("#text3").innerHTML = xhttp3.responseText;		
+		}
+	};
+	xhttp3.open("GET","textos/lasdb2017_meeting.txt", true);
+	xhttp3.send();
 }
 
