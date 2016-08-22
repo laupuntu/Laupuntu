eebcbabd3ac7af7a9326684ccadc9f0f6badb4e3
Daniel Alejandro Garavito Aguilar <danielgaravito@gmail.com>
2016-08-22 12:38:09 -0500

    Avance 5

--- Lasdb2017_project/js/interactions.js
+++ Lasdb2017_project/js/interactions.js
@@ -4,7 +4,7 @@
  
 		$("#owl-demo").owlCarousel({
  
-	    autoPlay: false, //Set AutoPlay to 3 seconds
+	    autoPlay: true, //Set AutoPlay to 3 seconds
 	    items : 1,
 	    itemsDesktop : [1199,1],
 	    itemsDesktopSmall : [979,1],
