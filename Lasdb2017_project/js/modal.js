7e08fd9c7d5cd5bab0325e460630a4c0291c0897
Daniel Alejandro Garavito Aguilar <danielgaravito@gmail.com>
2016-10-20 10:47:40 -0500

    Actualizacion Speakers Commite

--- Lasdb2017_project/js/modal.js
+++ Lasdb2017_project/js/modal.js
@@ -1098,3 +1098,28 @@ window.onclick = function(event) {
         modal43.style.display = "none";
     }
 }
+// Get the modal
+var modal44 = document.getElementById('myModal44');
+
+// Get the button that opens the modal
+var btn44 = document.getElementById("art44");
+
+// Get the <span> element that closes the modal
+var span44 = document.getElementsByClassName("close1")[35];
+
+// When the user clicks on the button, open the modal 
+btn44.onclick = function() {
+    modal44.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span44.onclick = function() {
+    modal44.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal44) {
+        modal44.style.display = "none";
+    }
+}
