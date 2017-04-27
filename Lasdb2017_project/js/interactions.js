5d4879e074d5930ed20b11ddc8dc1c29591d8fe2
Daniel Garavito <danielgaravito@gmail.com>
2017-04-27 17:01:28 -0500

    Listo Modal Pop Up

--- Lasdb2017_project/js/interactions.js
+++ Lasdb2017_project/js/interactions.js
@@ -18,7 +18,6 @@
   		});
  
 		});
-
 /* Funcion que permite tomar textos desde el servidor e inyectarlos en html*/
 loadText();
 function loadText() {
@@ -252,6 +251,39 @@ day[4].addEventListener("click", function(){
  });
 }
 );
+/* Configuracion Carrusel DIY*/
+(function(){
+  
+var counter = 0, // to keep track of current slide
+    $items = document.querySelectorAll('.diy-slideshow figure'), // a collection of all of the slides, caching for performance
+    numItems = $items.length; // total number of slides
+
+// this function is what cycles the slides, showing the next or previous slide and hiding all the others
+var showCurrent = function(){
+  var itemToShow = Math.abs(counter%numItems);// uses remainder (aka modulo) operator to get the actual index of the element to show  
+  
+  // remove .show from whichever element currently has it 
+  // http://stackoverflow.com/a/16053538/2006057
+  [].forEach.call( $items, function(el){
+    el.classList.remove('show');
+  });
+  
+  // add .show to the one item that's supposed to have it
+  $items[itemToShow].classList.add('show');    
+};
+
+// add click events to prev & next buttons 
+document.querySelector('.next').addEventListener('click', function() {
+     counter++;
+     showCurrent();
+  }, false);
+
+document.querySelector('.prev').addEventListener('click', function() {
+     counter--;
+     showCurrent();
+  }, false);
+  
+})();
 /*var acc = document.getElementsByClassName("accordion");
 
 acc[0].addEventListener("click", function(){
--- Lasdb2017_project/js/modal.js
+++ Lasdb2017_project/js/modal.js
@@ -223,7 +223,7 @@ window.onclick = function(event) {
         modal7.style.display = "none";
     }
 }
-/*// Get the modal
+// Get the modal
 var modal8 = document.getElementById('myModal8');
 
 // Get the button that opens the modal
@@ -247,7 +247,41 @@ window.onclick = function(event) {
     if (event.target == modal8) {
         modal8.style.display = "none";
     }
+}
+
+/*Modal Groups Discount Onload Page*/
+// Timer Modal Pop Up
+setTimeout(loadmodalsicount, 5000);
+// Get the modal
+var modal100 = document.getElementById('myModal100');
+
+// Get the button that opens the modal
+var btn100 = document.getElementsByTagName("body");
+
+// Get the <span> element that closes the modal
+var span100 = document.getElementsByClassName("close")[9];
+
+// When the user clicks on the button, open the modal 
+/*btn100.onload = function() {
+    modal42.style.display = "block";
 }*/
+function loadmodalsicount() {
+    modal100.style.display = "block";
+    modal100.style.zIndex = 1000;
+}
+// When the user clicks on <span> (x), close the modal
+span100.onclick = function() {
+    modal100.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal100) {
+        modal100.style.display = "none";
+    }
+}
+/********/
+
 // Get the modal
 var modal9 = document.getElementById('myModal9');
 
@@ -273,6 +307,7 @@ window.onclick = function(event) {
         modal9.style.display = "none";
     }
 }
+
 // Get the modal
 var modal10 = document.getElementById('myModal10');
 
@@ -1171,32 +1206,5 @@ span60.onclick = function() {
 window.onclick = function(event) {
     if (event.target == modal60) {
         modal60.style.display = "none";
-    }
-}
-//Modal Abstract Submission
-
-// Get the modal
-var modal = document.getElementById('myModal');
-
-// Get the button that opens the modal
-var btn = document.getElementById("btnsub");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
     }
 }
